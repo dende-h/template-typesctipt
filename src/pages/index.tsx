68f2d29d7b00afab78a0b994359d550dbe66aba3
Hiroya Shitogiden <dende.capo@gmail.com>
2022-10-05 11:23:16 +0900

    不要ライブラリアンインストール

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,10 @@
-import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
-import { getSupabase } from "../utils/supabase";
-
-
-const Index = ({ user, note }) => {
+const Index = () => {
 	
 	return (
 		<>
-		 	
+		 	<p>Hello World</p>
 		</>
 	);
 };
-
-export const getServerSideProps = withPageAuthRequired({
-	async getServerSideProps({ req, res }) {
-		const {
-			user: { accessToken }
-		} = getSession(req, res);
-
-		const supabase = getSupabase(accessToken);
-
-		const { data: note } = await supabase.from("note").select("*").order("created_at", { ascending: true });
-
-		return {
-			props: { note }
-		};
-	}
-});
 
 export default Index;
